cf2684a82384a327bb671083514842d10f3e4308
Mihail Stoica <90096221+mihail-stoica@users.noreply.github.com>
2022-10-05 11:27:17 +0200

    Adding Employee service, implementation and custom exception

---
+++ src/main/java/com/mihailstoica/springboot/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.mihailstoica.springboot.service;
+
+import com.mihailstoica.springboot.model.Employee;
+
+public interface EmployeeService {
+
+    Employee saveEmployee(Employee employee);
+}
---
+++ src/main/java/com/mihailstoica/springboot/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,30 @@
+package com.mihailstoica.springboot.service.impl;
+
+import com.mihailstoica.springboot.exception.ResourceNotFoundException;
+import com.mihailstoica.springboot.model.Employee;
+import com.mihailstoica.springboot.repository.EmployeeRepository;
+import com.mihailstoica.springboot.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+
+        Optional<Employee> savedEmployee = employeeRepository.findByEmail(employee.getEmail());
+        if (savedEmployee.isPresent()) {
+            throw new ResourceNotFoundException("Employee already exist with given email: " + employee.getEmail());
+        }
+
+        return employeeRepository.save(employee);
+    }
+}
