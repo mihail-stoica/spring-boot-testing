e697548ab1bc0386d1dae1bbd39e8937e5969b2a
Mihail Stoica <90096221+mihail-stoica@users.noreply.github.com>
2022-10-12 12:04:12 +0200

    Adding GetAllEmployee REST API & unit tests for it

--- src/main/java/com/mihailstoica/springboot/controller/EmployeeController.java
+++ src/main/java/com/mihailstoica/springboot/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.mihailstoica.springboot.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
@@ -21,5 +23,11 @@ public class EmployeeController {
     public Employee createEmployee(@RequestBody Employee employee) {
 
         return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+
+        return employeeService.getAllEmployees();
     }
 }
--- src/test/java/com/mihailstoica/springboot/controller/EmployeeControllerTests.java
+++ src/test/java/com/mihailstoica/springboot/controller/EmployeeControllerTests.java
@@ -12,9 +12,12 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -32,7 +35,6 @@ public class EmployeeControllerTests {
     @Autowired
     private ObjectMapper objectMapper;
 
-    //JUnit test for createEmployee
     @DisplayName("JUnit test for createEmployee")
     @Test
     public void givenEmployeeObject_whenCreateEmployee_thenReturnSavedEmployee() throws Exception {
@@ -58,6 +60,26 @@ public class EmployeeControllerTests {
                 .andExpect(jsonPath("$.firstName", is(employee.getFirstName())))
                 .andExpect(jsonPath("$.lastName", is(employee.getLastName())))
                 .andExpect(jsonPath("$.email", is(employee.getEmail())));
+    }
+
+    @DisplayName("JUnit test for getAllEmployees()")
+    @Test
+    public void givenListOfEmployee_whenGetAllEmployees_thenReturnEmployeesList() throws Exception {
+
+        //given - precondition or setup
+        List<Employee> listOfEmployees = List.of(
+                Employee.builder().firstName("John").lastName("Doe").email("john.doe@xyz,xyz").build(),
+                Employee.builder().firstName("Jane").lastName("Doe").email("jane.doe@xyz,xyz").build());
+        //stub method employeeService.getAllEmployees()
+        given(employeeService.getAllEmployees()).willReturn(listOfEmployees);
+
+        //when - action or behaviour that we are going to test
+        ResultActions response = mockMvc.perform(get("/api/employees"));
+
+        //then - verify the output
+        response.andExpect((status().isOk()))
+                .andDo(print())
+                .andExpect(jsonPath("$.size()", is(listOfEmployees.size())));
     }
 
 }
