33aa574b8580d59037156d65f1850c58325969a5
Mihail Stoica <90096221+mihail-stoica@users.noreply.github.com>
2022-10-15 17:33:01 +0200

    Add deleteEmployee REST API & unit tests

--- src/test/java/com/mihailstoica/springboot/controller/EmployeeControllerTests.java
+++ src/test/java/com/mihailstoica/springboot/controller/EmployeeControllerTests.java
@@ -18,6 +18,7 @@ import java.util.Optional;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -196,5 +197,21 @@ public class EmployeeControllerTests {
                 .andDo(print());
     }
 
+    @DisplayName("JUnit test for deleteEmployee")
+    @Test
+    public void givenEmployeeId_whenDeleteEmployee_thenReturn200() throws Exception {
 
+        //given - precondition or setup
+        long employeeId = 1L;
+
+        //stub method employeeService.getEmployeeById()
+        willDoNothing().given(employeeService).deleteEmployee(employeeId);
+
+        //when - action or behaviour that we are going to test
+        ResultActions response = mockMvc.perform(delete("/api/employees/{id}", employeeId));
+
+        //then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print());
+    }
 }
