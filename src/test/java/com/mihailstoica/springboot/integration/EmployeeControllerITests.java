005c52544b26c240886fd28f0bcfbccfe746ae7a
Mihail Stoica <90096221+mihail-stoica@users.noreply.github.com>
2022-10-17 11:42:18 +0200

    Add integration tests for updateEmployee positive & negative scenarios

--- src/test/java/com/mihailstoica/springboot/integration/EmployeeControllerITests.java
+++ src/test/java/com/mihailstoica/springboot/integration/EmployeeControllerITests.java
@@ -17,8 +17,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -119,6 +118,63 @@ public class EmployeeControllerITests {
     @DisplayName("JUnit test for getEmployeeById() negative scenario")
     @Test
     public void givenEmployeeId_whenGetEmployeeById_thenReturnEmpty() throws Exception {
+
+        //given - precondition or setup
+        long employeeId = 1L;
+        Employee employee = Employee.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("john.doe@xyz,xyz")
+                .build();
+
+        employeeRepository.save(employee);
+
+        //when - action or behaviour that we are going to test
+        ResultActions response = mockMvc.perform(get("/api/employees/{id}", employeeId));
+
+        //then - verify the output
+        response.andExpect(status().isNotFound())
+                .andDo(print());
+    }
+
+    //positive scenario - valid employee id
+    @DisplayName("JUnit test for updateEmployee() positive scenario")
+    @Test
+    public void givenEmployeeIdAndUpdatedEmployee_whenUpdateEmployee_thenReturnUpdatedEmployeeObject()
+            throws Exception {
+
+        //given - precondition or setup
+        Employee employee = Employee.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("john.doe@xyz,xyz")
+                .build();
+
+        Employee savedEmployee = employeeRepository.save(employee);
+
+        Employee updatedEmployee = Employee.builder()
+                .firstName("Jane")
+                .lastName("Doe")
+                .email("jane.doe@xyz,xyz")
+                .build();
+
+        //when - action or behaviour that we are going to test
+        ResultActions response = mockMvc.perform(put("/api/employees/{id}", savedEmployee.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedEmployee)));
+
+        //then - verify the output
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.firstName", is(updatedEmployee.getFirstName())))
+                .andExpect(jsonPath("$.lastName", is(updatedEmployee.getLastName())))
+                .andExpect(jsonPath("$.email", is(updatedEmployee.getEmail())));
+    }
+
+    //negative scenario - invalid employee id
+    @DisplayName("JUnit test for updateEmployee() negative scenario")
+    @Test
+    public void givenEmployeeIdAndUpdatedEmployee_whenUpdateEmployee_thenReturn404() throws Exception {
 
         //given - precondition or setup
         long employeeId = 1L;
